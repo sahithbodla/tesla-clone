57a0e334250aa3642b73b9f72f48b30162ddd054
Sahith Bodla <sahith.bodla@gmail.com>
2021-11-05 21:28:59 +0530

    Setting Section with Props

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import styled from 'styled-components'
 
-function Section() {
+function Section(props) {
     return (
-        <Wrap>
+        <Wrap bgImg={props.backgroundImg}>
             <ItemText>
-                <h1>Model S</h1>
-                <p>Order Online for Touchless Delivery</p>
+                <h1>{props.title}</h1>
+                <p>{props.description}</p>
             </ItemText>
             <Buttons>
                 <ButtonGroup>
                     <LeftButton>
-                        Custom Order
+                        {props.leftBtnText}
                     </LeftButton>
-                    <RightButton>
-                        Existing Inventory
-                    </RightButton>
+                    {props.rightBtnText && 
+                        <RightButton>
+                        {props.rightBtnText}
+                        </RightButton>
+                    }                    
                 </ButtonGroup>
                 <DownArrow src="/images/down-arrow.svg"/>
             </Buttons>
@@ -36,6 +38,7 @@ const Wrap = styled.div`
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
+    background-image: ${props => `url("/images/${props.bgImg}")`}
 `
 
 const ItemText = styled.div`
@@ -48,6 +51,9 @@ const Buttons = styled.div``
 const ButtonGroup = styled.div`
     display: flex;
     margin-bottom: 30px;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
 `
 const LeftButton = styled.div`
     background-color: rgba(23,26,32,0.8);
